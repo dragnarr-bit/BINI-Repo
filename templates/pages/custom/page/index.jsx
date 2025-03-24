4d4fcb9c6922b3ccbc4e49dd363b083576087a92
LiMayCode <lloyd.mark.parnecio@student.uno-r.edu.ph>
2025-03-24 15:25:21 +0800

    batman theme added

---
+++ templates/pages/custom/page/index.jsx
@@ -0,0 +1,234 @@
+import React from "react";
+import x11 from "./1-1.png";
+import Cd5Ae0Bd9D46D2A367982F154Ade6A400X1 from "./2-08cd5ae0-bd9d-46d2-a367-982f154ade6a-400x-1.png";
+import x21 from "./2-1.png";
+import E4A302E3Db12438780E5D779497D5B96400X1 from "./3-e4a302e3-db12-4387-80e5-d779497d5b96-400x-1.png";
+import x881 from "./88-1.png";
+import x882 from "./88-2.png";
+import x891 from "./89-1.png";
+import x901 from "./90-1.png";
+import x911 from "./91-1.png";
+import b587feca8b1a75c833f63b58b21 from "./959618b587feca8b1a75c833f63b58b2-1.png";
+import CRUNCH1 from "./CRUNCH-1.png";
+import { HeroButton } from "./HeroButton";
+import IMG7900400X1 from "./IMG-7900-400x-1.png";
+import IMG7902400X1 from "./IMG-7902-400x-1.png";
+import { IconComponentNode } from "./IconComponentNode";
+import { LogoMark } from "./LogoMark";
+import { MinusCircle } from "./MinusCircle";
+import { PlusCircle } from "./PlusCircle";
+import { Size48 } from "./Size48";
+import { StarFilled } from "./StarFilled";
+import { TopBarDefault } from "./TopBarDefault";
+import arrow1 from "./arrow-1.svg";
+import batmanCollectionpageHeader1080X2 from "./batman-collectionpage-header-1080x-2.png";
+import iconHeadstart262X190200X2001 from "./icon-headstart-262x190-200x200-1.png";
+import iconScreen262X190200X2001 from "./icon-screen-262x190-200x200-1.png";
+import iconYarn262X190200X2001 from "./icon-yarn-262x190-200x200-1.png";
+import "./style.css";
+
+export const ThemePages = () => {
+  return (
+    <div className="theme-pages">
+      <div className="div">
+        <img
+          className="icon-yarn"
+          alt="Icon yarn"
+          src={iconYarn262X190200X2001}
+        />
+
+        <img
+          className="icon-headstart"
+          alt="Icon headstart"
+          src={iconHeadstart262X190200X2001}
+        />
+
+        <img
+          className="icon-screen"
+          alt="Icon screen"
+          src={iconScreen262X190200X2001}
+        />
+
+        <header className="header">
+          <div className="navbar">
+            <div className="navbar-2">
+              <div className="home">
+                <div className="overlap-group">
+                  <img className="arrow" alt="Arrow" src={arrow1} />
+
+                  <div className="text-wrapper-3">Products</div>
+                </div>
+              </div>
+
+              <div className="div-wrapper">
+                <div className="text-wrapper-4">Blog</div>
+              </div>
+
+              <div className="home-2">
+                <div className="text-wrapper-5">Contact Us</div>
+              </div>
+
+              <div className="div-wrapper">
+                <div className="text-wrapper-6">FAQs</div>
+              </div>
+            </div>
+
+            <TopBarDefault className="top-bar-default-instance" />
+            <LogoMark className="logo-mark-instance" />
+          </div>
+
+          <img
+            className="batman"
+            alt="Batman"
+            src={batmanCollectionpageHeader1080X2}
+          />
+
+          <img className="element" alt="Element" src={x881} />
+
+          <div className="overlap">
+            <StarFilled className="star-filled" />
+            <StarFilled className="star-filled-instance" />
+            <StarFilled className="icon-instance-node" />
+            <StarFilled className="star-filled-2" />
+            <StarFilled className="star-filled-3" />
+          </div>
+
+          <p className="no-reviews-yet-write">
+            <span className="text-wrapper-7">(No reviews yet) </span>
+
+            <span className="text-wrapper-8">Write a review</span>
+          </p>
+
+          <div className="text-wrapper-9">Quantity:</div>
+
+          <div className="text-wrapper-10">1</div>
+
+          <p className="p">
+            <span className="text-wrapper-11">₱</span>
+
+            <span className="text-wrapper-12">4,500.00</span>
+          </p>
+
+          <MinusCircle className="minus-circle" />
+          <PlusCircle className="plus-circle" />
+          <div className="hero">Gotham-azing Batman™ Collector’s Box</div>
+        </header>
+
+        <div className="text-wrapper-13">Plus a head start</div>
+
+        <img className="img" alt="Element" src={x882} />
+
+        <div className="comes-with">
+          Comes with
+          <br />
+          everything you need
+        </div>
+
+        <div className="text-wrapper-14">Batman™ Crochet Kit</div>
+
+        <div className="text-wrapper-15">₱ 2,320.00</div>
+
+        <div className="text-wrapper-16">₱ 2,600.00</div>
+
+        <div className="text-wrapper-17">Harley Quinn™ Crochet Kit</div>
+
+        <p className="videos-for-every">
+          Videos for every step of <br />
+          the way
+        </p>
+
+        <HeroButton
+          className="hero-button-instance"
+          divClassName="design-component-instance-node"
+          text="BUY IT NOW"
+        />
+        <img className="element-2" alt="Element" src={x891} />
+
+        <img className="element-3" alt="Element" src={x21} />
+
+        <img className="element-4" alt="Element" src={x11} />
+
+        <p className="text-wrapper-18">
+          THE ONLY CROCHET KIT FOR COMPLETE BEGINNERS
+        </p>
+
+        <div className="overlap-2">
+          <div className="overlap-3">
+            <div className="text-wrapper-19">CHOOSE YOUR FAVORITE</div>
+
+            <div className="overlap-4">
+              <img
+                className="img-2"
+                alt="Img"
+                src={b587feca8b1a75c833f63b58b21}
+              />
+
+              <img className="element-5" alt="Element" src={x901} />
+            </div>
+          </div>
+
+          <img className="element-6" alt="Element" src={x911} />
+        </div>
+
+        <Size48 className="chevron-left" />
+        <IconComponentNode className="chevron-right" />
+        <img className="CRUNCH" alt="Crunch" src={CRUNCH1} />
+
+        <img className="IMG" alt="Img" src={IMG7900400X1} />
+
+        <img
+          className="element-cdae-bdd-d"
+          alt="Element"
+          src={Cd5Ae0Bd9D46D2A367982F154Ade6A400X1}
+        />
+
+        <img className="IMG-x" alt="Img" src={IMG7902400X1} />
+
+        <img
+          className="element-eae-db"
+          alt="Element"
+          src={E4A302E3Db12438780E5D779497D5B96400X1}
+        />
+
+        <p className="text-wrapper-20">SO EASY, EVERYONE CAN LEARN</p>
+
+        <p className="text-wrapper-21">WHO ARE THE THREAD AND CUPID?</p>
+
+        <p className="this-is-nami-dabest">
+          This is nami dabest chickenjoy
+          <br />I ever experienced kanami ka colors
+          <br />
+          <br />
+          -Mama J
+        </p>
+
+        <p className="not-only-are-our">
+          Not only are our crochet flowers crafted with love and compassion, but
+          every figure we create is imbued with the tender touch of thread and
+          Cupid. <br />
+          Each piece is carefully stitched—detail by detail—to ensure that every
+          creation is as unique as it is passionate. Whether it’s a cherished{" "}
+          <br />
+          gift or a keepsake for yourself, our handcrafted designs hold stories
+          meant to last forever.
+        </p>
+
+        <p className="this-is-nami-dabest-2">
+          This is nami dabest chickenjoy
+          <br />I ever experienced kanami ka colors
+          <br />
+          <br />
+          -Mama J
+        </p>
+
+        <p className="this-is-nami-dabest-3">
+          This is nami dabest chickenjoy
+          <br />I ever experienced kanami ka colors
+          <br />
+          <br />
+          -Mama J
+        </p>
+      </div>
+    </div>
+  );
+};
